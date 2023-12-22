6ad5bca852ec099c6d73597cfc33ae24e7e4cd57
RicardoJaraB <ricardo.jara@alumnos.ucn.cl>
2023-12-22 10:25:19 -0300

    sales fix

--- apps/frontend/src/pages/sales/index.tsx
+++ apps/frontend/src/pages/sales/index.tsx
@@ -2,8 +2,8 @@ import Grid from '@mui/material/Grid'
 import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
 import BarChart from 'src/views/dashboard/BarChart'
 import { useEffect, useState } from 'react'
-import { getProductQuery } from 'src/queries/productQuery'
 import LineChart from 'src/views/dashboard/LineChart'
+import { getSaleQuery } from 'src/queries/salesQuery'
 
 interface ResponseData {
   labels: string[],
@@ -19,18 +19,19 @@ const SalesPage = () => {
     const fetchData = async () => {
       try {
         const query: string = "SELECT productos.categoria, SUM(fact_ventas.cantidad) AS total_ventas FROM fact_ventas JOIN productos ON fact_ventas.id_producto = productos.identificador JOIN boletas ON fact_ventas.id_boleta = boletas.id JOIN tiempo ON boletas.id_tiempo = tiempo.id WHERE tiempo.año = 2022 GROUP BY productos.categoria ORDER BY total_ventas DESC;"
-        const datos = await getProductQuery(query);
+        const datos = await getSaleQuery(query);
         if (datos) {
           console.log("AQUI CATEGORIAS",datos);
           let lista_nombres: string[] = [];
           let lista_cantidad: number[] = [];
         
           for (const object of datos) {
-            lista_nombres.push(object.mes);
-            lista_cantidad.push(object.cantidad_stock);
+            lista_nombres.push(object.categoria);
+            lista_cantidad.push(object.total_ventas);
           }
         
           const lista: ResponseData = { labels: lista_nombres, amount: lista_cantidad };
+          console.log(lista)
           setSellsByCategory(lista);
         }
       } catch (error) {
@@ -45,7 +46,7 @@ const SalesPage = () => {
     const fetchData = async () => {
       try {
         const query: string = "SELECT tiempo.mes, SUM(fact_ventas.precio_total) AS ingresos_totales FROM tiempo JOIN boletas ON tiempo.id = boletas.id_tiempo JOIN fact_ventas ON boletas.id = fact_ventas.id_boleta WHERE tiempo.año = 2022 GROUP BY tiempo.mes ORDER BY tiempo.mes;"
-        const datos = await getProductQuery(query);
+        const datos = await getSaleQuery(query);
         if (datos) {
           console.log(datos);
           let lista_nombres: string[] = [];
@@ -58,7 +59,7 @@ const SalesPage = () => {
         
           for (const object of datos) {
             lista_nombres.push(object.mes);
-            lista_cantidad.push(object.cantidad_stock);
+            lista_cantidad.push(object.ingresos_totales);
           }
         
           const arrayOrdenado = lista_nombres.map((nombre, index) => ({ nombre, cantidad: lista_cantidad[index] }));
@@ -69,6 +70,7 @@ const SalesPage = () => {
           const lista_cantidad_ordenada = arrayOrdenado.map(item => item.cantidad);
         
           const lista: ResponseData = { labels: lista_nombres_ordenada, amount: lista_cantidad_ordenada };
+          console.log(lista)
           setMonthlyIncome(lista);
         }
       } catch (error) {
@@ -85,7 +87,7 @@ const SalesPage = () => {
           <BarChart title='Total de productos vendidos por categoría en el año 2022' seriesData={sellsByCategory.amount} categoriesData={sellsByCategory.labels} /> {/* ACA */}
         </Grid>
         <Grid item xs={12} md={6} lg={16}>
-          <LineChart title="Top 20 productos que generan más ganancia en el último mes" seriesData={monthlyIncome.amount} categoriesData={monthlyIncome.labels} /> {/* ACA */}
+          <LineChart title="Ingresos por mes en el año 2022" seriesData={monthlyIncome.amount} categoriesData={monthlyIncome.labels} /> {/* ACA */}
         </Grid>
       </Grid>
     </ApexChartWrapper>
